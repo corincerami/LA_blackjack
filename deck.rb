ea994a80c7003aa8420a7284c081184cb76efeb9
Chris C Cerami <chrisccerami@gmail.com>
2014-12-03 09:12:21 -0500

    Merging from Horizon repo

--- deck.rb
+++ deck.rb
@@ -1,7 +1,12 @@
 class Deck
   attr_reader :cards
-  def initialize(suits, numbers)
+  def initialize
     @cards = []
+
+    suits = ["hearts", "diamonds", "spades", "clubs"]
+    numbers = ["A", "2", "3", "4", "5", "6", "7",
+             "8", "9", "10", "J", "Q", "K"]
+
     suits.each do |suit|
       numbers.each do |number|
         @cards << Card.new(suit, number)
@@ -9,8 +14,4 @@ class Deck
     end
     @cards.shuffle!
   end
-
-  suits = ["hearts", "diamonds", "spades", "clubs"]
-  numbers = ["A", "2", "3", "4", "5", "6", "7",
-             "8", "9", "10", "J", "Q", "K"]
 end
--- hand.rb
+++ hand.rb
@@ -2,28 +2,35 @@ class Hand
   attr_reader :cards, :score
   def initialize(cards)
     @cards = cards
+    calculate_value
+  end
+
+  # i took out the argument from this method so it just calculates the
+  # total score, so it's calculating the score for the whole hand every
+  # time you're dealt a new card
+  def calculate_value
     @score = 0
     @cards.each do |card|
-      calculate_value(card)
+      if card.face_card?
+        @score += 10
+      elsif !card.ace?
+        @score += card.number.to_i
+      end
+    end
+    # i moved the called to ace_value out of the each loop, so that aces
+    # would always be tallied last.
+    @cards.each do |card|
+      aces_value(card) if card.ace?
     end
   end
 
-  def calculate_value(card)
-    if card.face_card?
-      value = 10
-    elsif card.ace?
-      value = aces_value(card)
-    else
-      value = card.number.to_i
-    end
-    @score += value
-  end
-
+  # this method and the one above now add directly to score rather than
+  # returning a value for each card.
   def aces_value(card)
-    if @score > 10 && card.ace?
-      1
-    elsif @score <= 10 && card.ace?
-      11
+    if @score > 10
+      @score += 1
+    elsif @score <= 10
+      @score += 11
     end
   end
 
